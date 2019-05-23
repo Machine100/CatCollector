f1ae82ce917b0369f5919777ed964c86a5c58afc
Machine100 <joesmileyyy@yahoo.com>
2019-05-23 15:34:35 -0400

    Adding in switchboard.

---
+++ src/switchboardclear.py
@@ -0,0 +1,25 @@
+
+import boto3
+
+dynamoresource=boto3.resource('dynamodb')
+table=dynamoresource.Table('switchboard')
+
+#response = table.get_item(Key={'switch':'cat1'})
+#subresponse = response['Item']
+#print (subresponse)
+#print (subresponse['status'])
+
+table.put_item(Item={'switch':'food1','status':False})
+table.put_item(Item={'switch':'food2','status':False})
+table.put_item(Item={'switch':'toy1','status':False})
+table.put_item(Item={'switch':'toy2','status':False})
+table.put_item(Item={'switch':'toy3','status':False})
+table.put_item(Item={'switch':'cat1','status':False})
+table.put_item(Item={'switch':'cat2','status':False})
+table.put_item(Item={'switch':'cat3','status':False})
+table.put_item(Item={'switch':'cat4','status':False})
+table.put_item(Item={'switch':'cat1','status':False})
+
+#response = table.get_item(Key={'switch':'cat5'})
+
+# table.put_item(Item={'Name':'BoYo','Age':'Baby'})
---
+++ src/switchboardwrite.py
@@ -0,0 +1,27 @@
+
+import boto3
+
+dynamoresource=boto3.resource('dynamodb')
+table=dynamoresource.Table('switchboard')
+
+#response = table.get_item(Key={'switch':'cat1'})
+#subresponse = response['Item']
+#print (subresponse)
+#print (subresponse['status'])
+
+table.put_item(Item={'switch':'food1','status':True})
+table.put_item(Item={'switch':'food2','status':True})
+table.put_item(Item={'switch':'toy1','status':True})
+table.put_item(Item={'switch':'toy2','status':True})
+table.put_item(Item={'switch':'toy3','status':True})
+table.put_item(Item={'switch':'cat1','status':True})
+table.put_item(Item={'switch':'cat2','status':True})
+table.put_item(Item={'switch':'cat3','status':True})
+table.put_item(Item={'switch':'cat4','status':True})
+table.put_item(Item={'switch':'cat1','status':True})
+
+#response = table.get_item(Key={'switch':'cat5'})
+
+# table.put_item(Item={'Name':'BoYo','Age':'Baby'})
+
+
